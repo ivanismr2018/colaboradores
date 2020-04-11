1ddace15bab38ae33318db5b517b014b87024f28
Sandy Fernando Perez Matamoros <sandypm007@gmail.com>
2020-04-11 12:07:00 -0400

    Added fixes to show the map

--- col/js/ayuda.js
+++ col/js/ayuda.js
@@ -1,25 +1,22 @@
+$.getJSON("data/municipios.geojson", function (muns) {
+    var locations = L.geoJson(muns);
 
-$.getJSON("data/municipios.geojson",
-    function (muns) {
-        locations = L.geoJson(muns);
-
-        var map_mun = L.map('mapa', {
-            center: [21.5, -79.371124],
-            zoom: 15,
-            layers: [locations],
-            keyboard: false,
-            dragging: true,
-            zoomControl: true,
-            boxZoom: false,
-            doubleClickZoom: false,
-            scrollWheelZoom: false,
-            tap: true,
-            touchZoom: true,
-            zoomSnap: 0.05,
-            maxBounds: geojsonM.getBounds()
-        });
-        map_mun.zoomControl.setPosition('topright');
-        map_mun.fitBounds(geojsonM.getBounds());
-
+    var map_mun = L.map('mapa', {
+        center: [21.5, -79.371124],
+        zoom: 15,
+        layers: [locations],
+        keyboard: false,
+        dragging: true,
+        zoomControl: true,
+        boxZoom: false,
+        doubleClickZoom: false,
+        scrollWheelZoom: false,
+        tap: true,
+        touchZoom: true,
+        zoomSnap: 0.05,
+        maxBounds: locations.getBounds()
+    });
+    map_mun.zoomControl.setPosition('topright');
+    map_mun.fitBounds(locations.getBounds());
 });
 
