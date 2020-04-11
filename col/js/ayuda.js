df591591d6909691812e313698736b3dd282c8ee
ivanismr2018 <38957591+ivanismr2018@users.noreply.github.com>
2020-04-11 18:00:28 +0200

    Add files via upload

---
+++ col/js/ayuda.js
@@ -0,0 +1,25 @@
+
+$.getJSON("data/municipios.geojson",
+    function (muns) {
+        locations = L.geoJson(muns);
+
+        var map_mun = L.map('mapa', {
+            center: [21.5, -79.371124],
+            zoom: 15,
+            layers: [locations],
+            keyboard: false,
+            dragging: true,
+            zoomControl: true,
+            boxZoom: false,
+            doubleClickZoom: false,
+            scrollWheelZoom: false,
+            tap: true,
+            touchZoom: true,
+            zoomSnap: 0.05,
+            maxBounds: geojsonM.getBounds()
+        });
+        map_mun.zoomControl.setPosition('topright');
+        map_mun.fitBounds(geojsonM.getBounds());
+
+});
+
