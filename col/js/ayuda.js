2fd9fe679c02514e8234213097481865c404358e
Sandy Fernando Perez Matamoros <sandypm007@gmail.com>
2020-04-11 12:21:08 -0400

    Added tootips and popups

--- col/js/ayuda.js
+++ col/js/ayuda.js
@@ -1,6 +1,18 @@
 $.getJSON("data/municipios.geojson", function (muns) {
     var locations = L.geoJson(muns);
 
+    locations.bindTooltip(function (layer) {
+        return '<span class="bd">' + layer.feature.properties.province + '</span> - ' + layer.feature.properties.municipality;
+    }, {'sticky': true});
+
+    locations.bindPopup(function (layer) {
+        let mun = layer.feature.properties.municipality;
+
+        return '<div class="small-pname"><span class="bd">' + mun + '</span></div>' +
+            '<div class="small-content"><span class="bd">Info extra</span></div>' +
+            '<div class="small-plink">&nbsp;</div>';
+    });
+
     var map_mun = L.map('mapa', {
         center: [21.5, -79.371124],
         zoom: 15,
